b311b42431ac505f0ab2964a43c33cf239c7ad2f
Houssem Nasri <housi.housi2015@gmail.com>
2021-02-28 15:59:30 +0100

    Document CostEntity class

--- src/main/java/tech/houssemnasri/api/CostEntity.java
+++ src/main/java/tech/houssemnasri/api/CostEntity.java
@@ -1,13 +1,31 @@
 package tech.houssemnasri.api;
 
+import java.util.Arrays;
+
+/**
+ * The {@code CostEntity} class holds the cost information of a node, some algorithms use cost
+ * values to determine the shortest path between a set of nodes
+ */
 public final class CostEntity {
+    /**
+     * We encourage creating a new {@code CostEntity} object every time cost values change rather
+     * than using an exiting one, this will help us listen for cost changes
+     */
     private final int[] costArguments;
 
     public CostEntity(final int[] costArguments) {
         this.costArguments = costArguments;
     }
 
+    /** Returns a list of calculated costs */
     public int[] getCostArguments() {
         return costArguments;
+    }
+
+    @Override
+    public String toString() {
+        return "CostEntity{" +
+                "costArguments=" + Arrays.toString(costArguments) +
+                '}';
     }
 }
