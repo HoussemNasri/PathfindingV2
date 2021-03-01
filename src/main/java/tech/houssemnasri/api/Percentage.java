74ded377960cfee8e9972de4dc44f44dad8009db
Houssem Nasri <housi.housi2015@gmail.com>
2021-03-01 23:40:46 +0100

    Improve Percentage class

--- src/main/java/tech/houssemnasri/api/Percentage.java
+++ src/main/java/tech/houssemnasri/api/Percentage.java
@@ -1,7 +1,8 @@
 package tech.houssemnasri.api;
 
+/** The {@code Percentage} class is used to deal with percentage of values */
 public class Percentage {
-    /** This is the value when the percentage is {@code 100%} */
+    /** This is the value when the percentage is at {@code 100%} */
     private final double fullValue;
 
     /** This is the value after taking some percentage of {@code fullValue} */
@@ -9,39 +10,41 @@ public class Percentage {
 
     public Percentage(final double fullValue, int initialPercentage) {
         this.fullValue = fullValue;
-        setPercentage(initialPercentage);
+        updateValue(initialPercentage);
     }
 
     public Percentage(final double fullValue) {
         this(fullValue, 100);
     }
 
+    /**
+     * Returns the full value
+     *
+     * @return the value when percentage is at{@code 100%}
+     */
     public double getFullValue() {
         return fullValue;
     }
 
-    public double getCurrentValue() {
-        return currentValue;
-    }
-
-    public void setPercentage(int percent) {
+    private void updateValue(int percent) {
         if (percent < 0 || percent > 100) {
             percent = Math.min(Math.max(percent, 0), 100);
         }
         currentValue = fullValue * (percent / 100.0d);
     }
 
-    public int getPercentage() {
-        return (int) (currentValue / fullValue) * 100;
-    }
-
-    public double setPercentageAndGet(int percent) {
-        setPercentage(percent);
+    /**
+     * Returns the percentage {@code percent} of the {@code fullValue}
+     *
+     * @return percentage from full value
+     */
+    public double getValue(int percent) {
+        updateValue(percent);
         return currentValue;
     }
 
     @Override
     public String toString() {
-        return String.format("%d%%", getPercentage());
+        return String.format("%d%%", (int) (currentValue / fullValue) * 100);
     }
 }
