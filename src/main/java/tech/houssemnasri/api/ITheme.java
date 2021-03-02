6d7dc9821e80934ab2e93d0bb113cd8fb39586f0
Houssem Nasri <housi.housi2015@gmail.com>
2021-03-02 23:43:38 +0100

    Implement PTheme for theme configurations

--- src/main/java/tech/houssemnasri/api/ITheme.java
+++ src/main/java/tech/houssemnasri/api/ITheme.java
@@ -1,4 +1,19 @@
 package tech.houssemnasri.api;
 
+import javafx.scene.paint.Color;
+
 public interface ITheme {
+    public Color getWallNodeColor();
+
+    public Color getOpenNodeColor();
+
+    public Color getClosedNodeColor();
+
+    public Color getBasicNodeColor();
+
+    public Color getPathNodeColor();
+
+    public Color getSourceNodeColor();
+
+    public Color getDestinationNodeColor();
 }
--- src/main/java/tech/houssemnasri/impl/PTheme.java
+++ src/main/java/tech/houssemnasri/impl/PTheme.java
@@ -1,5 +1,7 @@
 package tech.houssemnasri.impl;
 
+import javafx.scene.paint.Color;
+
 import tech.houssemnasri.api.ITheme;
 
 /**
@@ -7,5 +9,93 @@ import tech.houssemnasri.api.ITheme;
  * colors, fonts.., clients can use these info to decide how to display views.
  */
 public class PTheme implements ITheme {
+    private Color colorWallNode = Color.valueOf("#222021");
+    private Color colorOpenNode = Color.MEDIUMSEAGREEN;
+    private Color colorClosedNode = Color.INDIANRED;
+    private Color colorBasicNode = Color.IVORY;
+    private Color colorPathNode = Color.GOLD;
+    private Color colorSourceNode = Color.INDIGO;
+    private Color colorDestinationNode = Color.DEEPSKYBLUE;
 
+    private PTheme() {}
+
+    @Override
+    public Color getWallNodeColor() {
+        return colorWallNode;
+    }
+
+    @Override
+    public Color getOpenNodeColor() {
+        return colorOpenNode;
+    }
+
+    @Override
+    public Color getClosedNodeColor() {
+        return colorClosedNode;
+    }
+
+    @Override
+    public Color getBasicNodeColor() {
+        return colorBasicNode;
+    }
+
+    @Override
+    public Color getPathNodeColor() {
+        return colorPathNode;
+    }
+
+    @Override
+    public Color getSourceNodeColor() {
+        return colorSourceNode;
+    }
+
+    @Override
+    public Color getDestinationNodeColor() {
+        return colorDestinationNode;
+    }
+
+    public static class Builder {
+        private final PTheme instance = new PTheme();
+
+        public Builder setWallNodeColor(Color color) {
+            instance.colorWallNode = color;
+            return this;
+        }
+
+        public Builder setBasicNodeColor(Color color) {
+            instance.colorBasicNode = color;
+            return this;
+        }
+
+        public Builder setSourceNodeColor(Color color) {
+            instance.colorSourceNode = color;
+            return this;
+        }
+
+        public Builder setDestinationNodeColor(Color color) {
+            instance.colorDestinationNode = color;
+            return this;
+        }
+
+        public Builder setOpenNodeColor(Color color) {
+            instance.colorOpenNode = color;
+            return this;
+        }
+
+        public Builder setClosedNodeColor(Color color) {
+            instance.colorClosedNode = color;
+            return this;
+        }
+
+        public Builder setPathNodeColor(Color color) {
+            instance.colorPathNode = color;
+            return this;
+        }
+
+        public ITheme build() {
+            return instance;
+        }
+    }
+
+    enum Font {}
 }
