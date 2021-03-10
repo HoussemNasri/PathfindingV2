19db0f6be20529413b34725902d1e7202e1532a8
Houssem Nasri <housi.housi2015@gmail.com>
2021-03-10 23:01:52 +0100

    Create AstarCostAdapter to convert raw CostEntity to a specialized A* cost

---
+++ src/main/java/tech/houssemnasri/impl/algorithms/AstarCostAdapter.java
@@ -0,0 +1,41 @@
+package tech.houssemnasri.impl.algorithms;
+
+import tech.houssemnasri.CostEntity;
+import tech.houssemnasri.api.algorithms.cost.IAstarCost;
+
+public class AstarCostAdapter implements IAstarCost {
+    private final CostEntity costInfo;
+
+    public AstarCostAdapter(CostEntity costInfo) {
+        this.costInfo = costInfo;
+    }
+
+    @Override
+    public void updateGCost(int newGCost) {
+        costInfo.getCostArguments()[0] = newGCost;
+    }
+
+    @Override
+    public int gCost() {
+        return costInfo.getCostArguments()[0];
+    }
+
+    @Override
+    public void updateHCost(int newHCost) {
+        costInfo.getCostArguments()[1] = newHCost;
+    }
+
+    @Override
+    public int hCost() {
+        return costInfo.getCostArguments()[1];
+    }
+
+    public CostEntity getCostEntity() {
+        return costInfo;
+    }
+
+    @Override
+    public int compare(IAstarCost o1, IAstarCost o2) {
+        return Integer.compare(o1.fCost(), o2.fCost());
+    }
+}
